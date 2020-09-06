004d8d11ad693dbe8968a33e4b09bf334e133b65
zhaolonglong <QQ:1808056378 email:zhaolonglong@tydic.com>
2020-09-06 21:39:46 +0800

    fsf

---
+++ src/main/java/com/zlx/jenkins/controller/ooo.java
@@ -0,0 +1,13 @@
+package com.zlx.jenkins.controller;
+
+/**
+ * @Author Zhao LongLong
+ * @Date 2020/9/6
+ * @Version 1.0
+ * @Desc
+ */
+public class ooo {
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+    }
+}
